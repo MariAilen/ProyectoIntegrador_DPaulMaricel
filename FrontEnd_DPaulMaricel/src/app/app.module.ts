c17c5e649b711ca5e1989eac886c69cb992932dc
Maricel D'Paul <mari32.ailen@gmail.com>
2023-04-03 17:24:12 -0300

    Creacion del Header

--- FrontEnd_DPaulMaricel/src/app/app.module.ts
+++ FrontEnd_DPaulMaricel/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { APLogoComponent } from './components/ap-logo/ap-logo.component';
+import { SocialComponent } from './components/social/social.component';
+import { BannersComponent } from './components/banners/banners.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    APLogoComponent,
+    SocialComponent,
+    BannersComponent
   ],
   imports: [
     BrowserModule,
